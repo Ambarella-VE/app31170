6c435fda180e29a261b2197f0c8533c92c335bdd
Ambar Canonicco <64762162+Ambarella-VE@users.noreply.github.com>
2022-05-05 05:34:06 -0300

    Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import NavBar from "./components/navBar/NavBar";
+import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <NavBar>
+        <ItemListContainer greeting="Este es mi Item List Container" />
+        <div>
+          <p>
+            A tener en cuenta:
+          </p>
+            <ul>
+              <li>
+                Yo escribo mi app dentro de NavBar para que funcione bien el SideDrawer (es responsive).
+              </li>
+              <li>
+                Caso común escriben:
+              </li>
+              <div  style={{
+                backgroundColor: "#232323",
+                color: "#ffffff"
+              }}>
+
+                <code>
+                  <pre>
+{`
+  import NavBar from "./components/navBar/NavBar";
+  \n
+  import ItemListContainer from "./components/itemListContainer/ItemListContainer";
+  
+  function App() {
+    return (
+      <>
+        <NavBar>
+        <ItemListContainer greeting="Este es mi Item List Container" />
+      </>
+    );
+  }
+  
+  export default App;
+  `}
+                  </pre>
+                </code>
+              </div>
+          </ul>
+        </div>
+      </NavBar>
     </>
   );
 }
---
+++ src/components/itemListContainer/ItemListContainer.js
@@ -0,0 +1,7 @@
+export default function ItemListContainer({greeting}) {
+  return (
+    <>
+      <h1>{greeting}</h1>
+    </>
+  )
+}
